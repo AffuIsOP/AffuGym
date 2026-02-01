18f8abea302867f4cf0aa0c514c74212f1a13db0
AffuIsOP <safnan123456@gmail.com>
2026-02-01 19:09:45 +0500

    Initial commit - Flask Gym project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+@app.route('/about')
+def About():
+    return render_template('about.html')
+
+@app.route('/membership')
+def Membership():
+    return render_template('membership.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
